331d5426ad1aacfb23fba087d32ce27ac480be08
Nextus Marketing <marketingteamnextus@gmail.com>
2026-01-09 00:21:48 +0530

    changes done

--- resources/views/Frontend/home.blade.php
+++ resources/views/Frontend/home.blade.php
@@ -33,7 +33,7 @@
           <h1
             class="uppercase text-base lg:text-xl font-semibold animate__animated animate__fadeInUp"
           >
-           Welcome to Accords Book
+           Welcome to Accords Bookkkkkkkkkk
 </h1>
           <div class="display-2">
           Reliable  
